3a688823bad63269da40361d1275238efd4c3e3e
Edgar Haro <nicolas.meli-at-912053765133>
2021-08-23 01:05:32 -0400

    pokemons sotted

--- src/containers/pokemon-list/PokemonList.tsx
+++ src/containers/pokemon-list/PokemonList.tsx
@@ -5,18 +5,26 @@ import PokemonDetail from "../pokemon-detail/PokemonDetail";
 
 const PokemonList = () => {
   const dispatch = useDispatch();
-  const {results: pokemons} = useSelector((state: any) => state.pokemon.data);
-  const {isLoading} = useSelector((state: any) => state.pokemon);
+  const { results: pokemons } = useSelector((state: any) => state.pokemon.data);
+  const { isLoading } = useSelector((state: any) => state.pokemon);
+
+  const pokeSort = () => {
+    return pokemons.sort((pokeA: any, pokeB: any) => {
+      return pokeA.name > pokeB.name ? 1 : -1;
+    });
+  };
 
   useEffect(() => {
-    if (isLoading === 'iddle')
-      dispatch(pokemonFetch());
+    if (isLoading === "iddle") dispatch(pokemonFetch());
   }, []);
 
   return (
     <div>
-      {pokemons&&pokemons.length > 0 &&
-        pokemons.map((pokemon: any) => <PokemonDetail key={pokemon.url} pokemon={pokemon}/>)}
+      {pokemons &&
+        pokemons.length > 0 &&
+        pokeSort().map((pokemon: any) => (
+          <PokemonDetail key={pokemon.url} pokemon={pokemon} />
+        ))}
     </div>
   );
 };
